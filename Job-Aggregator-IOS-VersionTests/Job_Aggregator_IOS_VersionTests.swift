019fd3fe608f1e17209bed0b3761ed26350c15dc
Alexander <alexsahnykov@gmail.com>
2018-08-03 11:06:04 +0300

    git init

---
+++ Job-Aggregator-IOS-VersionTests/Job_Aggregator_IOS_VersionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Job_Aggregator_IOS_VersionTests.swift
+//  Job-Aggregator-IOS-VersionTests
+//
+//  Created by Александр Сахнюков on 03/08/2018.
+//  Copyright © 2018 Александр Сахнюков. All rights reserved.
+//
+
+import XCTest
+@testable import Job_Aggregator_IOS_Version
+
+class Job_Aggregator_IOS_VersionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
